50689e8a22c6e974f642dc6d1ee28b75634ce7c5
Juergen Daubert <jue@jue.li>
2010-05-29 16:30:15 +0200

    Updated copyright notices to 2010.

--- pkgutil.cc
+++ pkgutil.cc
@@ -2,7 +2,7 @@
 //  pkgutils
 // 
 //  Copyright (c) 2000-2005 Per Liden
-//  Copyright (c) 2006-2007 by CRUX team (http://crux.nu)
+//  Copyright (c) 2006-2010 by CRUX team (http://crux.nu)
 // 
 //  This program is free software; you can redistribute it and/or modify
 //  it under the terms of the GNU General Public License as published by
